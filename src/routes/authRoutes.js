69bc9d50754eac28e221ab982b620b5bd1cebb12
Lavanya Prasad <lavanyaprasad2804@gmail.com>
2026-05-22 09:36:27 +0530

    completed authentication module

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,25 @@
+import express from "express";
+import authMiddleware from "../middleware/authMiddleware.js";
+
+import {
+  registerUser,
+  loginUser,
+} from "../controllers/authController.js";
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+
+router.post("/login", loginUser);
+
+router.get("/profile", authMiddleware, (req, res) => {
+
+  res.status(200).json({
+    success: true,
+    message: "Protected Route Accessed",
+    user: req.user,
+  });
+
+});
+
+export default router;
