6f40680e7cd8bdbb7672f3661231d016f6c3a9a5
21A91A05H3 <21A91A05H3@aec.edu.in>
2026-05-24 22:08:16 +0530

    sidebar updated

--- client/src/components/Sidebar/sidebar.js
+++ client/src/components/Sidebar/sidebar.js
@@ -11,7 +11,14 @@ import {
   FaSignOutAlt
 } from "react-icons/fa";
 
+import {
+  Link,
+  useLocation
+} from "react-router-dom";
+
 function Sidebar() {
+
+  const location = useLocation();
 
   return (
 
@@ -23,39 +30,132 @@ function Sidebar() {
 
       <ul className="sidebar-menu">
 
-        <li>
-          <FaTachometerAlt className="icon" />
-          Dashboard
-        </li>
+        <Link
+          to="/dashboard"
 
-        <li>
-          <FaCashRegister className="icon" />
-          POS
-        </li>
+          className={
+            location.pathname === "/dashboard"
+            ?
+            "active"
+            :
+            ""
+          }
+        >
+          <li>
 
-        <li>
-          <FaBoxOpen className="icon" />
-          Products
-        </li>
+            <FaTachometerAlt className="icon" />
 
-        <li>
-          <FaWarehouse className="icon" />
-          Inventory
-        </li>
+            Dashboard
 
-        <li>
-          <FaShoppingCart className="icon" />
-          Orders
-        </li>
+          </li>
+        </Link>
 
-        <li>
-          <FaUsers className="icon" />
-          Customers
-        </li>
+        <Link
+          to="/pos"
+
+          className={
+            location.pathname === "/pos"
+            ?
+            "active"
+            :
+            ""
+          }
+        >
+          <li>
+
+            <FaCashRegister className="icon" />
+
+            POS
+
+          </li>
+        </Link>
+
+        <Link
+          to="/products"
+
+          className={
+            location.pathname === "/products"
+            ?
+            "active"
+            :
+            ""
+          }
+        >
+          <li>
+
+            <FaBoxOpen className="icon" />
+
+            Products
+
+          </li>
+        </Link>
+
+        <Link
+          to="/inventory"
+
+          className={
+            location.pathname === "/inventory"
+            ?
+            "active"
+            :
+            ""
+          }
+        >
+          <li>
+
+            <FaWarehouse className="icon" />
+
+            Inventory
+
+          </li>
+        </Link>
+
+        <Link
+          to="/orders"
+
+          className={
+            location.pathname === "/orders"
+            ?
+            "active"
+            :
+            ""
+          }
+        >
+          <li>
+
+            <FaShoppingCart className="icon" />
+
+            Orders
+
+          </li>
+        </Link>
+
+        <Link
+          to="/customers"
+
+          className={
+            location.pathname === "/customers"
+            ?
+            "active"
+            :
+            ""
+          }
+        >
+          <li>
+
+            <FaUsers className="icon" />
+
+            Customers
+
+          </li>
+        </Link>
 
         <li className="logout">
+
           <FaSignOutAlt className="icon" />
+
           Logout
+
         </li>
 
       </ul>
