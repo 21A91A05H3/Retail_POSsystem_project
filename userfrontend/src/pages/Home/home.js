fb3e1a203cb3712147e7cd0fd3430e4ba386d9f7
21A91A05H3 <21A91A05H3@aec.edu.in>
2026-06-06 22:35:58 +0530

    added product static

--- userfrontend/src/pages/Home/home.js
+++ userfrontend/src/pages/Home/home.js
@@ -13,17 +13,91 @@ function Home() {
   const [products, setProducts] =
     useState([]);
 
+  const [cart, setCart] =
+    useState([]);
+
   useEffect(() => {
 
-    const storedProducts =
+    const storedProducts = [
 
-      JSON.parse(
-        localStorage.getItem("products")
-      ) || [];
+      {
+        id: 1,
+        name: "Tea",
+        price: 20
+      },
+
+      {
+        id: 2,
+        name: "Coffee",
+        price: 50
+      },
+
+      {
+        id: 3,
+        name: "Milk",
+        price: 40
+      },
+
+      {
+        id: 4,
+        name: "Rice",
+        price: 100
+      }
+    ];
 
     setProducts(storedProducts);
 
   }, []);
+
+  const addToCart = (product) => {
+
+    const existingProduct =
+
+      cart.find(
+        (item) =>
+          item.id === product.id
+      );
+
+    let updatedCart;
+
+    if(existingProduct){
+
+      updatedCart =
+
+        cart.map((item) =>
+
+          item.id === product.id
+          ?
+          {
+            ...item,
+            quantity: item.quantity + 1
+          }
+          :
+          item
+        );
+    }
+    else{
+
+      updatedCart = [
+
+        ...cart,
+
+        {
+          ...product,
+          quantity: 1
+        }
+      ];
+    }
+
+    setCart(updatedCart);
+
+    localStorage.setItem(
+      "cart",
+      JSON.stringify(updatedCart)
+    );
+
+    alert(`${product.name} added to cart`);
+  };
 
   return (
 
@@ -68,93 +142,6 @@ function Home() {
         <div className="row">
 
           {
-            products.length === 0
-            ?
-
-           <div className="row">
-
-  <div className="col-md-3 mb-4">
-
-    <div className="card product-card">
-
-      <div className="card-body text-center">
-
-        <h5>
-          Milk
-        </h5>
-
-        <p>
-          ₹50
-        </p>
-
-        <button className="btn btn-primary">
-
-          Add to Cart
-
-        </button>
-
-      </div>
-
-    </div>
-
-  </div>
-
-  <div className="col-md-3 mb-4">
-
-    <div className="card product-card">
-
-      <div className="card-body text-center">
-
-        <h5>
-          Bread
-        </h5>
-
-        <p>
-          ₹40
-        </p>
-
-        <button className="btn btn-primary">
-
-          Add to Cart
-
-        </button>
-
-      </div>
-
-    </div>
-
-  </div>
-
-  <div className="col-md-3 mb-4">
-
-    <div className="card product-card">
-
-      <div className="card-body text-center">
-
-        <h5>
-          Rice
-        </h5>
-
-        <p>
-          ₹100
-        </p>
-
-        <button className="btn btn-primary">
-
-          Add to Cart
-
-        </button>
-
-      </div>
-
-    </div>
-
-  </div>
-
-</div>
-
-            :
-
             products.map((product) => (
 
               <div
@@ -178,7 +165,13 @@ function Home() {
 
                     </p>
 
-                    <button className="btn btn-primary">
+                    <button
+                      className="btn btn-primary"
+
+                      onClick={() =>
+                        addToCart(product)
+                      }
+                    >
 
                       Add to Cart
 
